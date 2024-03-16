d99993d4b184aa20f5cdff45384c193d389e0269
DevangEDBA <devang.mestry@edba.io>
2024-03-16 17:23:36 +0530

    COMMIT_2

---
+++ frontend/src/SpeedometerComponent.js
@@ -0,0 +1,12 @@
+// SpeedometerComponent.jsx
+import React from 'react';
+
+const SpeedometerComponent = () => {
+  return (
+    <div className="speedometer-content">
+      <div className="speedometer-box">This is Speedometer</div>
+    </div>
+  );
+};
+
+export default SpeedometerComponent;
